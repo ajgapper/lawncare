e0cf003a64ffa687f051e5c9973de6bcab5cef0a
Restyled.io <commits@restyled.io>
2020-05-06 01:46:58 +0000

    Restyled by prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,16 @@
 // graphql function doesn't throw an error so we have to check to check for the
 // result.errors to throw manually
-const _ = require('lodash')
-const path = require('path')
-const {createFilePath} = require('gatsby-source-filesystem')
-const {fmImagesToRelative} = require('gatsby-remark-relative-images')
+const _ = require("lodash");
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 
-exports.createPages =
-    async ({graphql, actions}) => {
-  const {createPage} = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-  return graphql(`{
-   allMarkdownRemark  {
+  return graphql(`
+    {
+      allMarkdownRemark {
         edges {
           node {
             id
@@ -24,66 +24,65 @@ exports.createPages =
         }
       }
     }
-    `).then(result => {
+  `).then((result) => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
+      result.errors.forEach((e) => console.error(e.toString()));
+      return Promise.reject(result.errors);
     }
 
-    const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges;
 
-      posts.forEach(edge => {
-        const id = edge.node.id
-        createPage({
-          path: edge.node.frontmatter.slug,
-          categories: edge.node.frontmatter.categories,
-          component: path.resolve(
-           `src/templates/${String(edge.node.frontmatter.templateKey)}.tsx`
-          ),
-          context: {
-            id: edge.node.id,
-            category: edge.node.frontmatter.category,
-          },
-        })
-      })
+    posts.forEach((edge) => {
+      const id = edge.node.id;
+      createPage({
+        path: edge.node.frontmatter.slug,
+        categories: edge.node.frontmatter.categories,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.tsx`
+        ),
+        context: {
+          id: edge.node.id,
+          category: edge.node.frontmatter.category,
+        },
+      });
+    });
 
-        let categories = []
-        // Iterate through each webpage, putting all found categories into
-        // `categories`
-        posts.forEach(edge => {
-          if (_.get(edge, `node.frontmatter.categories`)) {
-            categories = categories.concat(edge.node.frontmatter.categories)
-          }
-        })
-        // Eliminate duplicate categories
-        categories = _.uniq(categories)
+    let categories = [];
+    // Iterate through each webpage, putting all found categories into
+    // `categories`
+    posts.forEach((edge) => {
+      if (_.get(edge, `node.frontmatter.categories`)) {
+        categories = categories.concat(edge.node.frontmatter.categories);
+      }
+    });
+    // Eliminate duplicate categories
+    categories = _.uniq(categories);
 
-        // Make categories pages
-  categories.forEach(categories => {
-    const categoriesPath = `/categories/${_.kebabCase(categories)}/`
+    // Make categories pages
+    categories.forEach((categories) => {
+      const categoriesPath = `/categories/${_.kebabCase(categories)}/`;
 
-    createPage({
-      path: categoriesPath,
-      component: path.resolve(`src/templates/categories.tsx`),
-      context: {
-        categories,
-      },
-    })
-  })
-  })
-}
+      createPage({
+        path: categoriesPath,
+        component: path.resolve(`src/templates/categories.tsx`),
+        context: {
+          categories,
+        },
+      });
+    });
+  });
+};
 
-                                  exports.onCreateNode =
-        ({node, actions, getNode}) => {
-          const {createNodeField} = actions
-          fmImagesToRelative(node) // convert image paths for gatsby images
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  fmImagesToRelative(node); // convert image paths for gatsby images
 
-          if (node.internal.type === `MarkdownRemark`) {
-            const value = createFilePath({node, getNode})
-            createNodeField({
-              name : `slug`,
-              node,
-              value,
-            })
-          }
-        }
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
--- src/html.js
+++ src/html.js
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from "prop-types";
+import React from "react";
 
 export default function HTML(props) {
   return (
@@ -8,7 +8,7 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
-  name = "viewport"
+          name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
@@ -16,7 +16,11 @@ export default function HTML(props) {
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
-        <noscript>This website requires JavaScript. To contact us, please send us an email at: <a href="mailto:agapper@gappsapps.co.uk">agapper@gappsapps.co.uk</a></noscript>
+        <noscript>
+          This website requires JavaScript. To contact us, please send us an
+          email at:{" "}
+          <a href="mailto:agapper@gappsapps.co.uk">agapper@gappsapps.co.uk</a>
+        </noscript>
         <div
           key={`body`}
           id="___gatsby"
@@ -25,14 +29,14 @@ export default function HTML(props) {
         {props.postBodyComponents}
       </body>
     </html>
-  )
+  );
 }
 
 HTML.propTypes = {
-  htmlAttributes : PropTypes.object,
-  headComponents : PropTypes.array,
-  bodyAttributes : PropTypes.object,
-  preBodyComponents : PropTypes.array,
-  body : PropTypes.string,
-  postBodyComponents : PropTypes.array,
-}
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
