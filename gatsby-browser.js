1a2e2e7f357a86481cec6ed9b32995ee31d5964e
Restyled.io <commits@restyled.io>
2020-05-11 02:03:53 +0000

    Restyled by clang-format

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import Layout from './src/components/layout';
 import 'lazysizes'
 
-export const wrapPageElement = ({ element }) => {
+import React from 'react';
+
+import Layout from './src/components/layout';
+
+export const wrapPageElement = ({element}) => {
   return <Layout>{element}</Layout>;
 };
 
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,6 @@
 import React from 'react';
 import Layout from './src/components/layout';
 
-export const wrapPageElement = ({ element }) => {
+export const wrapPageElement = ({element}) => {
   return <Layout>{element}</Layout>;
 };
